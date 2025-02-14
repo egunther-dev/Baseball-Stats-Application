c55174549428cda118c38f34571408c01cdc1588
egunther-dev <evanm.gunther@gmail.com>
2025-02-14 11:07:30 -0500

    base

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-from pybaseball import  playerid_lookup
-from pybaseball import  statcast_pitcher
-playerid_lookup('kershaw', 'clayton')
-kershaw_stats = statcast_pitcher("2017-02-01","2025-01-01",477132)
-kershaw_stats.groupby("pitch_type").release_speed.agg("mean")
-print(kershaw_stats)
+from pybaseball import playerid_lookup, pitching_stats
+from pybaseball import statcast_pitcher
+
+def get_pitcher_stats():
+    pitcher_last = input("Enter pitchers last name: ").strip()
+    pitcher_first = input("Enter pitchers first name: ").strip()
+
+    player_info = playerid_lookup(pitcher_last, pitcher_first)
+
+    if player_info.empty:
+        print("Player not found!")
+        return
+    print(player_info)
+
+    player_id = player_info.iloc[0]['key_mlbam']
+    print(f"MLBAM ID for {pitcher_first} {pitcher_last}: {player_id}")
+
+
+    start_season = input("Enter Start Season: ")
+    end_season = input("Enter End Season: ")
+
+    stats = pitching_stats(start_season, end_season)
+
+    pitcher_stats = stats[stats['IDfg'] == player_id]
+    if pitcher_stats.empty:
+        print(f"No stats found for {pitcher_first} {pitcher_last} in {start_season}-{end_season}")
+        return
+
+    selected_columns = ['Name', 'Team', 'Season', 'W', 'L', 'ERA', 'WAR', 'SO', 'WHIP']
+    print(pitcher_stats[selected_columns])
+
+
+
+if __name__ == "__main__":
+    get_pitcher_stats()
